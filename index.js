a0fff99012f66ba188371c9b9f721fdbcd6fb3dd
CharaeKeow <34004186+CharaeKeow@users.noreply.github.com>
2018-11-27 23:43:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,14 @@
-class Clock extends React.Component {  
-  constructor(props) {
-    super(props);
-    /*This is where Clock initializes this.state with an object to
-      include the current time.*/
-    this.state = {date: new Date()}; //Reminder: this.state is only
-    //be assigned in the constructor.
-  }
-  
-  //Will be called when Clock output is inserted in the DOM.
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),1000 //Call tick method once a second
-    );
-  }
-  
-  //Will be called if Clock component is ever removed from the DOM.
-  componentWillUnmount() {
-    clearInterval(this.timerID); //Clear the timer.
-  }
-  
-  
-  /*Called every second. Inside, the clock component schedules a UI
-    update by calling setState() + object containing the current time.  
-  */
-  tick() {
-    this.setState({ 
-      date: new Date()      
-    });
-  }
-  
-  /*And tick() is called every second, so render() method will also be 
-    called every second.Just this.state.date in the render() is 
-    different,so the render output will include the updated time.
-    React updates the DOM accordingly.
-  */
-  
-  render() {
-    return (
-      <div className="border">
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>   
-      </div>
-    );
-  }
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+
+  ReactDOM.render(
+    element, document.getElementById('root')
+  );
 }
 
-ReactDOM.render(
-  <Clock />,
-  document.getElementById('root')
-);
+setInterval(tick, 1000) //render (or update UI, in this sense), every 1 second. 
